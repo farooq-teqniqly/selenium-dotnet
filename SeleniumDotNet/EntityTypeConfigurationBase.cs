2f9c0dfda6478d2a26efb2ffdee6eceb8c25cdc7
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2021-10-21 13:39:07 -0700

    Wip (#1)

---
+++ SeleniumDotNet/EntityTypeConfigurationBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SeleniumDotNet
+{
+    public abstract class EntityTypeConfigurationBase<TEntity> : IEntityTypeConfiguration<TEntity>
+        where TEntity : class
+    {
+        public void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            // This configuration applies to ALL entities.
+            builder.Property<DateTime>("_Created")
+                .IsRequired()
+                .HasDefaultValueSql("GETUTCDATE()")
+                .ValueGeneratedOnAdd();
+            
+            this.FurtherConfiguration(builder);
+        }
+
+        public abstract void FurtherConfiguration(EntityTypeBuilder<TEntity> builder);
+    }
+}
--- SeleniumDotNet/WebDriverFactory.cs
+++ SeleniumDotNet/WebDriverFactory.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using OpenQA.Selenium.Chrome;
 
 namespace SeleniumDotNet
@@ -14,7 +13,13 @@ namespace SeleniumDotNet
             string driverPath,
             TimeSpan timeout)
         {
+            var options = new ChromeOptions();
+            options.AddArgument("user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36");
             var driver = new ChromeDriver(driverPath);
+            driver.Manage().Timeouts().ImplicitWait = timeout;
+            driver.Manage().Window.Maximize();
+
+          var driver = new ChromeDriver(driverPath);
             driver.Manage().Timeouts().ImplicitWait = timeout;
 
             return driver;
